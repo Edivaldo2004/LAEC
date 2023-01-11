34e6680bec2470cc36a8ce4d96074f82e3d64264
Edivaldo José dos Santos Junior <101206689+Edivaldo2004@users.noreply.github.com>
2023-01-11 20:12:25 +0000

    Site terminado

--- script.js
+++ script.js
@@ -16,13 +16,24 @@ function redirect() {
 }
 
 function card() {
-  let name = document.getElementById("name").value
-  let numer = document.getElementById("numer").value
-  let age = document.getElementById("age").value
-  window.location.href = "informcartao.html"
+  let name = document.getElementById("name").value;
+  let numer = document.getElementById("numer").value;
+  let age = document.getElementById("age").value;
 
 
-  let imprimir = document.getElementById("imprimir").innerHTML = name;
-  let imprimir2 = document.getElementById("imprimir2").innerHTML = numer;
-  let imprimir3 = document.getElementById("imprimir3").innerHTML = age;
+  let imprimir = document.getElementById("imprimir").innerHTML = "Nome: " + name;
+  let imprimir2 = document.getElementById("imprimir2").innerHTML = "Numero da Conta: " + numer;
+  let imprimir3 = document.getElementById("imprimir3").innerHTML = "Agência: " + age;
+}
+function confirme() {
+  var x;
+  var r = confirm("Pagamento Confirmado");
+  if (r == true) {
+    x = "você pressionou OK!";
+  }
+  else {
+    x = "Você pressionou Cancelar!";
+  }
+  window.location.href = "confirm.html"
+  document.getElementById("demo").innerHTML = x;
 }
